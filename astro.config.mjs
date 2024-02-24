18d3c519a0d1ebf6c4b5e2550e2633af6a785022
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-02-24 22:31:23 +0300

    disable ssr - doesnt work (

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 import alpine from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  // output: "server",
+  // adapter: vercel(),
+  compressHTML: true,
+  build: {
+    assets: "assets",
+    inlineStylesheets: "never",
+  },
   integrations: [
     alpine({
       entrypoint: "/src/entrypoint",
